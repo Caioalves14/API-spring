eb7d960d1ce22178ebea3f6e3ceb420652bd0569
Caioalves14 <caio.alvesrex@gmail.com>
2023-08-01 21:46:36 -0300

    Spring Boot Project

---
+++ src/test/java/com/springBoot/CaioAlves/CaioAlvesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springBoot.CaioAlves;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaioAlvesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
